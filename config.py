738e075e5b7d9d192e4eff02ae38870cf2c31c37
Thundercat <michael.c.haines@gmail.com>
2015-02-11 08:21:10 -0700

    cleaner config

--- config.py
+++ config.py
@@ -1,5 +1,2 @@
-SECRET_KEY = 'You will never guess the key'
-
 import os
 base_path = os.path.abspath(os.path.dirname(__file__))
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_path, 'sales.db')
